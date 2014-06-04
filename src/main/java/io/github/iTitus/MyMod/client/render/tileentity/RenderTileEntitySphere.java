b58e444c0283bb7bbc874c69664f15ae67efa9e2
iTitus <mil.vel@web.de>
2014-06-04 19:13:53 +0200

    Start on a sphere

--- src/main/java/io/github/iTitus/MyMod/client/render/tileentity/RenderTileEntitySphere.java
+++ src/main/java/io/github/iTitus/MyMod/client/render/tileentity/RenderTileEntitySphere.java
@@ -1,26 +1,88 @@
 package io.github.iTitus.MyMod.client.render.tileentity;
 
+import io.github.iTitus.MyMod.helper.MathHelper;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
 import net.minecraft.tileentity.TileEntity;
 
+import org.lwjgl.opengl.GL11;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
+@SideOnly(Side.CLIENT)
 public class RenderTileEntitySphere extends TileEntitySpecialRenderer {
 
 	public static final RenderTileEntitySphere INSTANCE = new RenderTileEntitySphere();
 
 	@Override
 	public void renderTileEntityAt(TileEntity tile, double x, double y,
-			double z, float ticks) {
-		
-		/* Spherical coordinates ;) Have to be drawn by GL
-		 * 
-		 * x = "radius" * sin("Angle1") * cos("Angle2")
-		 * y = "radius" * sin("Angle1") * sin("Angle2")
-		 * z = "radius" * cos("Angle1")
-		 * 
-		 * "Angle1" = [0;"pi"] / [0°;180°]
-		 * "Angle2" = [0;2"pi"[ / [0°;360°[ OR ]-"pi";"pi"] / ]-180°;180°]
-		 */
+			double z, float partialTicks) {
+
+		float radius = 0.75F;
+
+		GL11.glPushMatrix();
+
+		GL11.glDisable(GL11.GL_LIGHTING);
+		GL11.glDisable(GL11.GL_TEXTURE_2D);
+		GL11.glEnable(GL11.GL_LINE_SMOOTH);
+		GL11.glEnable(GL11.GL_BLEND);
+		GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+		GL11.glHint(GL11.GL_LINE_SMOOTH_HINT, GL11.GL_NICEST);
+		GL11.glHint(GL11.GL_POLYGON_SMOOTH_HINT, GL11.GL_NICEST);
+
+		GL11.glColor4f(1F, 1F, 1F, 1F);
+
+		double posX = Minecraft.getMinecraft().renderViewEntity.lastTickPosX
+				+ (Minecraft.getMinecraft().renderViewEntity.posX - Minecraft
+						.getMinecraft().renderViewEntity.lastTickPosX)
+				* partialTicks;
+		double posY = Minecraft.getMinecraft().renderViewEntity.lastTickPosY
+				+ (Minecraft.getMinecraft().renderViewEntity.posY - Minecraft
+						.getMinecraft().renderViewEntity.lastTickPosY)
+				* partialTicks;
+		double posZ = Minecraft.getMinecraft().renderViewEntity.lastTickPosZ
+				+ (Minecraft.getMinecraft().renderViewEntity.posZ - Minecraft
+						.getMinecraft().renderViewEntity.lastTickPosZ)
+				* partialTicks;
+
+		double translation = MathHelper
+				.sin((Minecraft.getMinecraft().renderViewEntity.ticksExisted + partialTicks) / 10.0F) * 0.1F + 0.1F;
+
+		GL11.glTranslated(-posX, -posY, -posZ);
+
+		GL11.glBegin(GL11.GL_LINE_LOOP);
+
+		// for (int i = 0; i < 360; i++) {
+		// GL11.glVertex3d(radius * MathHelper.sin(MathHelper.degToRad(i))
+		// + tile.xCoord + 0.5, tile.yCoord + 1,
+		// radius * MathHelper.cos(MathHelper.degToRad(i))
+		// + tile.zCoord + 0.5);
+		// }
+
+		for (float angle1 = 0; angle1 <= 180; angle1++) {
+
+			for (float angle2 = 0; angle2 < 360; angle2++) {
+				GL11.glVertex3d(
+						radius * MathHelper.sin(MathHelper.degToRad(angle1))
+								* MathHelper.cos(MathHelper.degToRad(angle2))
+								+ tile.xCoord + 0.5,
+						radius * MathHelper.sin(MathHelper.degToRad(angle1))
+								* MathHelper.sin(MathHelper.degToRad(angle2))
+								+ tile.yCoord + 1.5 + translation, radius
+								* MathHelper.cos(MathHelper.degToRad(angle1))
+								+ tile.zCoord + 0.5);
+			}
+		}
+
+		GL11.glEnd();
+
+		GL11.glEnable(GL11.GL_LIGHTING);
+		GL11.glEnable(GL11.GL_TEXTURE_2D);
+		GL11.glDisable(GL11.GL_LINE_SMOOTH);
+		GL11.glDisable(GL11.GL_BLEND);
+
+		GL11.glPopMatrix();
 
 	}
-
 }
---
+++ src/main/java/io/github/iTitus/MyMod/helper/MathHelper.java
@@ -0,0 +1,19 @@
+package io.github.iTitus.MyMod.helper;
+
+public class MathHelper {
+
+	public static final float PI = (float) Math.PI;
+
+	public static float degToRad(float deg) {
+		return (deg / 180F) * PI;
+	}
+
+	public static float sin(float f) {
+		return net.minecraft.util.MathHelper.sin(f);
+	}
+
+	public static float cos(float f) {
+		return net.minecraft.util.MathHelper.cos(f);
+	}
+
+}
