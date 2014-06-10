434e0b6b1a796cfcf1e40979a51619965cfb704e
iTitus <mil.vel@web.de>
2014-06-10 15:38:11 +0200

    Tweak for the rendering of the zero block

--- src/main/java/io/github/iTitus/MyMod/block/zero/BlockZero.java
+++ src/main/java/io/github/iTitus/MyMod/block/zero/BlockZero.java
@@ -2,10 +2,14 @@ package io.github.iTitus.MyMod.block.zero;
 
 import io.github.iTitus.MyMod.block.EnumBlockType;
 import io.github.iTitus.MyMod.block.MyBlock;
+import net.minecraft.block.Block;
 import net.minecraft.entity.EnumCreatureType;
 import net.minecraft.util.AxisAlignedBB;
+import net.minecraft.util.Facing;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 
 public class BlockZero extends MyBlock {
 
@@ -20,9 +24,15 @@ public class BlockZero extends MyBlock {
 	}
 
 	@Override
-	public AxisAlignedBB getCollisionBoundingBoxFromPool(World par1World,
-			int x, int y, int z) {
+	public AxisAlignedBB getCollisionBoundingBoxFromPool(World world, int x,
+			int y, int z) {
 		return null;
+	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public int getRenderBlockPass() {
+		return 0;
 	}
 
 	@Override
@@ -33,6 +43,28 @@ public class BlockZero extends MyBlock {
 	@Override
 	public boolean renderAsNormalBlock() {
 		return false;
+	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public boolean shouldSideBeRendered(IBlockAccess world, int x, int y,
+			int z, int side) {
+
+		Block block = world.getBlock(x, y, z);
+
+		if (world.getBlockMetadata(x, y, z) != world.getBlockMetadata(x
+				- Facing.offsetsXForSide[side], y
+				- Facing.offsetsYForSide[side], z
+				- Facing.offsetsZForSide[side])) {
+			return true;
+		}
+
+		if (block == this) {
+			return false;
+		}
+
+		return block == this ? false : super.shouldSideBeRendered(world, x, y,
+				z, side);
 	}
 
 }
