e51db4ff9ebbb6ca0ce7ef15c4dd6811811387a6
iTitus <mil.vel@web.de>
2014-06-10 15:53:30 +0200

    Sooooo...

--- src/main/java/io/github/iTitus/MyMod/client/gui/GUIClockConfig.java
+++ src/main/java/io/github/iTitus/MyMod/client/gui/GUIClockConfig.java
@@ -3,7 +3,10 @@ package io.github.iTitus.MyMod.client.gui;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.util.StatCollector;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 
+@SideOnly(Side.CLIENT)
 public class GUIClockConfig extends GuiScreen {
 
 	public GUIClockConfig() {
@@ -13,8 +16,9 @@ public class GUIClockConfig extends GuiScreen {
 	@Override
 	public void drawScreen(int x, int y, float partialTicks) {
 		drawDefaultBackground();
-		drawCenteredString(fontRendererObj, StatCollector.translateToLocal("gui.clockConfig.name"), width / 2,
-				40, 16777215);
+		drawCenteredString(fontRendererObj,
+				StatCollector.translateToLocal("gui.clockConfig.name"),
+				width / 2, 40, 16777215);
 		super.drawScreen(x, y, partialTicks);
 	}
 
