c056026b9ce0f119698ff32483f3d98112292fab
iTitus <mil.vel@web.de>
2014-06-16 11:26:18 +0200

    Now I just need to change the version in build.gradle

--- src/main/java/io/github/iTitus/MyMod/lib/LibMod.java
+++ src/main/java/io/github/iTitus/MyMod/lib/LibMod.java
@@ -4,6 +4,6 @@ public class LibMod {
 
 	public static final String MODID = "mymod";
 	public static final String NAME = "iTitus' MyMod";
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "@VERSION@";
 
 }
